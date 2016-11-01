4829a10cade70da65722a81d5197e5314690caa8
Casper Sørensen <casp677d@edu.easj.dk>
2016-11-01 12:48:42 +0100

    simple lommeregner done

--- lommeregner_pair/MainPage.xaml.cs
+++ lommeregner_pair/MainPage.xaml.cs
@@ -29,37 +29,48 @@ namespace lommeregner_pair
 
         public double tal1 { get; set; }
         public double tal2 { get; set; }
+
+        public double tal3 { get; set; }
+        public double tal4 { get; set; }
+
         public double mem { get; set; }
 
         private void btn_plus_Click(object sender, RoutedEventArgs e)
         {
             TextboxTilVariable();
             tb_visResultat.Text = (tal1 + tal2).ToString();
+            tb_operator.Text = "+";
         }
 
         private void btn_minus_Click(object sender, RoutedEventArgs e)
         {
             TextboxTilVariable();
             tb_visResultat.Text = (tal1 - tal2).ToString();
+            tb_operator.Text = "-";
         }
 
         private void btn_gange_Click(object sender, RoutedEventArgs e)
         {
             TextboxTilVariable();
             tb_visResultat.Text = (tal1 * tal2).ToString();
+            tb_operator.Text = "*";
         }
 
         private void btn_divider_Click(object sender, RoutedEventArgs e)
         {
             TextboxTilVariable();
             tb_visResultat.Text = (tal1 / tal2).ToString();
+            tb_operator.Text = "/";
         }
 
         private void btn_ryd_Click(object sender, RoutedEventArgs e)
         {
             tb_tal1.Text = "";
             tb_tal2.Text = "";
-            tb_visResultat.Text = "";
+            tal1 = 0;
+            tal2 = 0;
+            tb_operator.Text = "";
+            tb_visResultat.Text = "slettet";
         }
 
         private void TextboxTilVariable()
@@ -71,18 +82,20 @@ namespace lommeregner_pair
         private void btn_m_Click(object sender, RoutedEventArgs e)
         {
             mem = Int32.Parse(tb_visResultat.Text);
+            tb_visResultat.Text = mem + " :er gemt";
             tb_tal1.Text = mem.ToString();
             tb_tal2.Text = "";
         }
 
         private void btn_mRyd_Click(object sender, RoutedEventArgs e)
         {
-            
+            //clearer alt
             tb_tal1.Text = "";
-            tb_tal2.Text = "";
             mem = 0;
             tb_visResultat.Text = "";
+         }
 
-        }
+
+
     }
 }
