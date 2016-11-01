6633348996a46f2e3eebfbda62cd41e679c3a8fd
Casper Sørensen <casp677d@edu.easj.dk>
2016-11-01 11:25:28 +0100

    lavede en memory knap og 2 slet knapper

--- lommeregner_pair/MainPage.xaml.cs
+++ lommeregner_pair/MainPage.xaml.cs
@@ -27,33 +27,61 @@ namespace lommeregner_pair
             this.InitializeComponent();
         }
 
+        public double tal1 { get; set; }
+        public double tal2 { get; set; }
+        public double mem { get; set; }
+
         private void btn_plus_Click(object sender, RoutedEventArgs e)
         {
-
+            TextboxTilVariable();
+            tb_visResultat.Text = (tal1 + tal2).ToString();
         }
 
         private void btn_minus_Click(object sender, RoutedEventArgs e)
         {
-
+            TextboxTilVariable();
+            tb_visResultat.Text = (tal1 - tal2).ToString();
         }
 
         private void btn_gange_Click(object sender, RoutedEventArgs e)
         {
-
+            TextboxTilVariable();
+            tb_visResultat.Text = (tal1 * tal2).ToString();
         }
 
         private void btn_divider_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void btn_resultat_Click(object sender, RoutedEventArgs e)
-        {
-
+            TextboxTilVariable();
+            tb_visResultat.Text = (tal1 / tal2).ToString();
         }
 
         private void btn_ryd_Click(object sender, RoutedEventArgs e)
         {
+            tb_tal1.Text = "";
+            tb_tal2.Text = "";
+            tb_visResultat.Text = "";
+        }
+
+        private void TextboxTilVariable()
+        {
+            tal1 = Int32.Parse(tb_tal1.Text);
+            tal2 = Int32.Parse(tb_tal2.Text);
+        }
+
+        private void btn_m_Click(object sender, RoutedEventArgs e)
+        {
+            mem = Int32.Parse(tb_visResultat.Text);
+            tb_tal1.Text = mem.ToString();
+            tb_tal2.Text = "";
+        }
+
+        private void btn_mRyd_Click(object sender, RoutedEventArgs e)
+        {
+            
+            tb_tal1.Text = "";
+            tb_tal2.Text = "";
+            mem = 0;
+            tb_visResultat.Text = "";
 
         }
     }
